17d15dffb04cd5f352800b189cdba6a7c3b15881
Krosebrook <kylerosebrook@gmail.com>
2025-07-15 07:22:00 -0500

    wip saving

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 module.exports = {
   env: {
     es6: true,
@@ -25,4 +26,35 @@ module.exports = {
     },
   ],
   globals: {},
+=======
+// functions/.eslintrc.js
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    tsconfigRootDir: __dirname,
+    project: ['./tsconfig.json', './tsconfig.dev.json'],
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  ignorePatterns: [
+    'node_modules/',
+    // skip nested or non‑TS entrypoints
+    '**/*.js',
+  ],
+  overrides: [
+    {
+      // for your JS files, fall back to the default parser (no TS project)
+      files: ['*.js'],
+      parser: 'espree',
+      parserOptions: {
+        project: null,
+      },
+    },
+  ],
+>>>>>>> c688649 (codelab stastarting point)
 };
